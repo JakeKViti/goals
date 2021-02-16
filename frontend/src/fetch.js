1d6211a2e5b37821f6229413b84c278e77ecde3d
JakeKViti <thevitij64@gmail.com>
2021-02-16 16:44:38 -0500

    fetch calls were moved to fetch.js and made it able to call them from different files

--- frontend/src/fetch.js
+++ frontend/src/fetch.js
@@ -1,14 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const BASE_URL = "http://localhost:3000/api/v1"
-    const USERS_URL = `${BASE_URL}/users`
-    const GOALS_URL = `${BASE_URL}/goals`
-    
-    fetch(USERS_URL)
+class FetchCalls {
+    constructor () {
+    this.baseURL = "http://localhost:3000/api/v1"
+    this.userURL = `${this.baseURL}/users`
+    this.goalURL = `${this.baseURL}/goals`
+    }
+
+    consoleLogUsers(){
+    fetch(this.userURL)
     .then(resp => resp.json())
     .then(users => (console.log(users)));
+    }
 
-    fetch(GOALS_URL)
+    consoleLogGoals(){
+    fetch(this.goalURL)
     .then(resp => resp.json())
     .then(goals => (console.log(goals)));
-    })
-    
+    }
+
+}
