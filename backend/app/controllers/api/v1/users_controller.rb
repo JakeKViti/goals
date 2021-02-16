6740c3a0f9acac6f2b260d8f64e71f91aa5bfca0
JakeKViti <thevitij64@gmail.com>
2021-02-16 00:21:23 -0500

    create index and create methods for user controller

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::UsersController < ApplicationController
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def create
+        user = User.create(user_params)
+        render json: user
+    end
+
+    private
+
+    def goal_params
+        params.require(:problem).permit(
+            :name
+        )
+    end
 end
