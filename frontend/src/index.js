e4e1d16b111cc6c4cbdaf161cc9032238ca19293
JakeKViti <thevitij64@gmail.com>
2021-02-16 17:51:54 -0500

    let javascript console log the user's name for debugging

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,11 @@ const sss = new FetchCalls();
 sss.consoleLogUsers()
 
 let username = document.getElementById('user-form')
-let nammer = document.getElementById('name-input')
+
 //let username = document.getElementById('name-input')
 
 username.addEventListener("submit" , function(e){ 
     e.preventDefault()
+    let nammer = document.getElementById('name-input')
+    console.log(nammer.value)
 })
-//debugger
