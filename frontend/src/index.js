50bc4fce9a4caa172dc37c1c1399538e605bc1e4
JakeKViti <thevitij64@gmail.com>
2021-02-16 17:38:35 -0500

    added basic form and listener for input

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,11 @@
 const sss = new FetchCalls();
+sss.consoleLogUsers()
+
+let username = document.getElementById('user-form')
+let nammer = document.getElementById('name-input')
+//let username = document.getElementById('name-input')
+
+username.addEventListener("submit" , function(e){ 
+    e.preventDefault()
+})
+//debugger
