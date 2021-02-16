75a92a68da93f7ced56006f7f137db7d0afb7376
JakeKViti <thevitij64@gmail.com>
2021-02-16 00:17:02 -0500

    methods created for goalscontroller

--- backend/app/controllers/api/v1/goals_controller.rb
+++ backend/app/controllers/api/v1/goals_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::GoalsController < ApplicationController
+    def index
+        goals = Goal.all
+        render json: goals
+    end
+
+    def create
+        goal = Goal.create(goal_params)
+        render json: goal
+    end
+
+    def destory
+        goal = Goal.find(params[:id]).destroy
+        render json: goal
+    end
+
+    private
+
+    def goal_params
+        params.require(:problem).permit(
+            :name, 
+            :user_id)
+    end
 end
