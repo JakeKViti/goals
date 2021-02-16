57cb1bf1b7b06502fdf3d0ed6944d0655fea821b
JakeKViti <thevitij64@gmail.com>
2021-02-15 23:59:27 -0500

    inital generation of goals and users tables

--- backend/db/migrate/20210216044006_create_goals.rb
+++ backend/db/migrate/20210216044006_create_goals.rb
@@ -1,7 +1,9 @@
 class CreateGoals < ActiveRecord::Migration[6.0]
   def change
     create_table :goals do |t|
-
+      t.string :title
+      t.boolean :completed
+      t.belongs_to :user
       t.timestamps
     end
   end
