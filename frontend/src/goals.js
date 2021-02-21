224747c6e09942684350d430f8cdc95d2054d72f
JakeKViti <thevitij64@gmail.com>
2021-02-20 19:55:17 -0500

    user goals are saved into a class array

--- frontend/src/goals.js
+++ frontend/src/goals.js
@@ -1,12 +1,17 @@
 class Goal {
+
+    static userGoals = []
+
     constructor(goal){
         this.title = goal.title
         this.id = goal.id
         this.user_id = goal.user_id
+        Goal.userGoals.push(goal)
     }
 
     displayGoals(){
         let geol = document.getElementById('test')
         geol.innerText = `${this.title}`
+        debugger
     }
 }
