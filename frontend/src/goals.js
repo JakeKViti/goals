1ffc6eadbc1ade6f9778aed944c47d3d868d8453
JakeKViti <thevitij64@gmail.com>
2021-02-23 23:13:14 -0500

    Goal class tracks goal.completed

--- frontend/src/goals.js
+++ frontend/src/goals.js
@@ -5,6 +5,7 @@ class Goal {
     constructor(goal){
         this.title = goal.title
         this.id = goal.id
+        this.completed = goal.completed
         this.user_id = goal.user_id
         Goal.userGoals.push(goal)
     }
